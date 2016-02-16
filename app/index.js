e79a9e9243610b36513802e52c4ed0c448ce1bb9
Eleanor Weigert <eleanor.weigert@byhook.com>
2016-02-16 13:16:28 -0800

    Add animated background and comets

--- app/index.js
+++ app/index.js
@@ -2,4 +2,10 @@
 
 var config = require('./config');
 
-TweenMax.to(".nyan-cat", 5, {left: 600, border: 'green', color: 'transparent'});
+TweenMax.to(".nyan-cat", 5, {x: 500, y: -100});
+
+TweenMax.to(".container", 15, {backgroundPosition: "100% -30%", repeat: -1, repeatDelay: 0, ease: Linear.easeNone});
+
+
+TweenMax.to(".comet1", 1, {x: -1600, y: 400, opacity: 0, repeat: -1, repeatDelay: 1.5, ease: Linear.easeNone});
+TweenMax.to(".comet2", 1, {x: -1300, y: 400, opacity: 1, repeat: -1, delay: 2, repeatDelay: 2});
