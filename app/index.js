7622908f084de28cb82bf6e2b3e8777def42781d
Eleanor Weigert <eleanor.weigert@byhook.com>
2016-02-17 10:33:51 -0800

    Flatten out motion of space / comets / nyan to zero slope

--- app/index.js
+++ app/index.js
@@ -12,21 +12,23 @@ var text1 = document.getElementById('text1');
 var text2 = document.getElementById('text2');
 var textContainer = document.getElementsByClassName('text-container');
 
-var spaceTween = TweenMax.to(space, 35, {backgroundPosition: '-100% 73%', repeat: -1, repeatDelay: -1, ease: Linear.easeNone});
-var nyanTween = TweenMax.to(nyan, 10, {x: 600, y: -100});
-var comet1Tween = TweenMax.to(comet1, 1, {x: -1600, y: 300, opacity: 0, repeat: -1, repeatDelay: 2.5, ease: Linear.easeNone});
-var comet2Tween = TweenMax.to(comet2, 1, {x: -1300, y: 400, opacity: 1, repeat: -1, delay: 3, repeatDelay: 3});
+var spaceTween = TweenMax.to(space, 13, {backgroundPosition: '-100%', repeat: -1, repeatDelay: -1, ease: Linear.easeNone});
+var nyanTween = TweenMax.to(nyan, 10, {x: 500, y: 0});
+var comet1Tween = TweenMax.to(comet1, 1, {x: -1600, y: 0, opacity: 0, repeat: -1, repeatDelay: 1.5, ease: Linear.easeNone});
+var comet2Tween = TweenMax.to(comet2, 1, {x: -1300, y: 0, opacity: 1, repeat: -1, delay: 1, repeatDelay: 2.5});
 
 var tl = new TimelineMax();spaceTween;
 
+/*
 tl.to(textContainer, 0.25, {opacity: 1}, 0)
-  .from(text1, 4, {opacity:0})
+  .from(text1, 1, {opacity:0})
   .to(text1, 1.5, {opacity: 0})
-  .from(text2, 4, {opacity: 0})
+  .from(text2, 1, {opacity: 0})
   .to(text2, 1.5, {opacity: 0})
   .to(space, 2, {opacity: 1})
   .append(nyanTween, -2)
   .insert(comet1Tween, 0)
   .insert(comet2Tween, 0);
-
+*/
 //TweenMax.to(pizza, 2, {opacity: 0, repeat: -1, yoyo: true, delay: 0});
+
