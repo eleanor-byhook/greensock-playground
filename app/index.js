602d80877cf8e152406900c42f407733c28c5b32
Eleanor Weigert <eleanor.weigert@byhook.com>
2016-02-18 12:54:59 -0800

    basic space parallax background

--- app/index.js
+++ app/index.js
@@ -7,7 +7,9 @@ var $ = require('jquery');
 var nyan  = $('.nyan-cat');
 var space = $('.background-container');
 var background1 = $('#background1');
+var background1b = $('#background1b');
 var background2 = $('#background2');
+var background2b = $('#background2b');
 var background3 = $('#background3');
 var comet1 = $('.comet1');
 var comet2 = $('.comet2');
@@ -28,17 +30,21 @@ $(document).on('mousemove', function(e) {
 /* Scrolling background */
 
 //based on this example: http://netgfx.com/trunk/scrollingBG/
+//RED
 var left1 = background1.position().left;
 console.log('left: ' + left1);
 TweenMax.to(background1,7,{css:{left: left1-1976},repeatDelay:0,useFrames:false,ease: Linear.easeNone, repeat:-1});
 
+
+//BLUE
 var left2 = background2.position().left;
 console.log('left2: ' + left2);
-TweenMax.to(background2,7,{css:{left: left2-1976},repeatDelay:0,useFrames:false,ease: Linear.easeNone,repeat:-1});
+TweenMax.to(background2,14,{css:{left: left2-1976},repeatDelay:0,useFrames:false,ease: Linear.easeNone,repeat:-1});
 
+//GREEN
 var left3 = background3.position().left;
 console.log('left3: '+ left3);
-TweenMax.to(background3,7,{css:{left: left3-1976},repeatDelay:0,useFrames:false,ease: Linear.easeNone,repeat:-1});
+TweenMax.to(background3,21,{css:{left: left3-1976},repeatDelay:0,useFrames:false,ease: Linear.easeNone,repeat:-1});
 
 /* Comets */
 var comet1Tween = TweenMax.to(comet1, 1, {x: -1600, y: 0, opacity: 0, repeat: -1, repeatDelay: 1.5, ease: Linear.easeNone});
@@ -54,7 +60,7 @@ tl.to(textContainer, 1, {opacity: 1}, 0)
   .from(text2, 3, {opacity: 0})
   .to(text2, 1.5, {opacity: 0})
   .to(space, 1, {opacity: 1})
-  .to([nyan,comet1Tween, comet2Tween], 2, {display: 'inline'})
+  .to([nyan, comet1Tween, comet2Tween], 2, {display: 'inline'})
   .insert(comet1Tween, 0)
   .insert(comet2Tween, 0);
 
