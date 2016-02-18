e772dc445287e880c51113e48f1785d76d20898a
Eleanor Weigert <eleanor.weigert@byhook.com>
2016-02-17 16:53:29 -0800

    space scrolling using large re-ordered background tiles

--- app/index.js
+++ app/index.js
@@ -5,7 +5,10 @@ var config = require('./config');
 var $ = require('jquery');
 
 var nyan  = $('.nyan-cat');
-var space = $('.container');
+//var space = $('.background-container');
+var background1 = $('#background1');
+var background2 = $('#background2');
+var background3 = $('#background3');
 var comet1 = $('.comet1');
 var comet2 = $('.comet2');
 var pizza = $('.pizza');
@@ -13,9 +16,35 @@ var text1 = $('#text1');
 var text2 = $('#text2');
 var textContainer = $('.text-container');
 
-//var spaceTween = TweenMax.to(space, 8, {backgroundPosition: '-100%', repeat: -1, repeatDelay: 0, ease: Power0.easeNone});
+$(document).on('mousemove', mouse);
+/*
+function tweenAngle(degrees) {
+  var x = 0;
+  var y = 0;
+  switch(degrees) {
+    case degrees >= -90 && degrees < 45:
+          return
+  }
+};
+*/
+
+//based on this example: http://netgfx.com/trunk/scrollingBG/
+var left1 = background1.position().left;
+console.log('left: ' + left1);
+TweenMax.to(background1,7,{css:{left: left1-1976},repeatDelay:0,useFrames:false,ease: Linear.easeNone, repeat:-1});
+
+var left2 = background2.position().left;
+console.log('left2: ' + left2);
+TweenMax.to(background2,7,{css:{left: left2-1976},repeatDelay:0,useFrames:false,ease: Linear.easeNone,repeat:-1});
+
+var left3 = background3.position().left;
+console.log('left3: '+ left3);
+TweenMax.to(background3,7,{css:{left: left3-1976},repeatDelay:0,useFrames:false,ease: Linear.easeNone,repeat:-1});
+
+
+//var spaceTween = TweenMax.to(space, 8, {backgroundPosition: '-1% 99%', repeat: -1, repeatDelay: 0, ease: Power0.easeNone});
 //var nyanTween = TweenMax.to(nyan, 10, {x: 350, y: 0});
-//var comet1Tween = TweenMax.to(comet1, 1, {x: -1600, y: 0, opacity: 0, repeat: -1, repeatDelay: 1.5, ease: Linear.easeNone});
+//var comet1Tween = TweenMax.to(comet1, 1, {x: -1600, y: 0, opacity: 0, repeat: -1, repeatDelay: 1.5, ease: ease: Linear.easeNone});
 //var comet2Tween = TweenMax.to(comet2, 1, {x: -1300, y: 0, opacity: 1, repeat: -1, delay: 1, repeatDelay: 2.5});
 
 var tl = new TimelineMax();
@@ -26,14 +55,12 @@ tl.to(textContainer, 0.25, {opacity: 1}, 0)
   .to(text1, 1.5, {opacity: 0})
   .from(text2, 1, {opacity: 0})
   .to(text2, 1.5, {opacity: 0})
-  .to(space, 2, {opacity: 1})
-  .append(nyanTween, -2)
+  .to(spaceTween, 2, {opacity: 1})
   .insert(comet1Tween, 0)
   .insert(comet2Tween, 0);
+
+TweenMax.to(pizza, 2, {opacity: 0, repeat: -1, yoyo: true, delay: 0});
 */
-//TweenMax.to(pizza, 2, {opacity: 0, repeat: -1, yoyo: true, delay: 0});
-
-
 // console.log("Y: " + event.pageY);
 
 function mouse(event) {
@@ -51,9 +78,6 @@ function mouse(event) {
       $(this).css('-o-transform', 'rotate(' + degree + 'deg)');
       $(this).css('-ms-transform', 'rotate(' + degree + 'deg)');
     });
-    console.log(degree);
+    //console.log(degree);
   }
-}
-$(document).on('mousemove', mouse);
-
-
+};
